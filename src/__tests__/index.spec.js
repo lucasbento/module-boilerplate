4ef8a895bc2ca5f748947c28e3798c84ce4573c6
Lucas Bento <lucas.bsilva@outlook.com>
2017-10-03 09:36:16 +0200

    Initial commit

---
+++ src/__tests__/index.spec.js
@@ -0,0 +1,5 @@
+import calculateArgs from '../index';
+
+it('should calculate args', () => {
+  expect(calculateArgs(2, 2)).toBe(4);
+});
---
+++ src/index.js
@@ -0,0 +1,2 @@
+export default (arg1, arg2) =>
+  arg1 + arg2;
